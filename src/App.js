50610d0d45e8e22a78c86460fec125ad893293be
Barbara <barbara.vieira99@gmail.com>
2021-11-21 21:25:27 +0000

    App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import Hero from "./components/Hero/Hero";
+import Gallery from "./components/Gallery/Gallery";
+import Hamburger from "./components/Hamburguer/Hamburger";
+import ReactDOM from "react-dom";
+import Sidebar from './components/Sidebar/Sidebar';
+
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import Activities from "./components/Activities/Activities";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar/>
+
+      <BrowserRouter>
+       
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Hero/>}/>
+            <Route path="/gallery" element={<Gallery/>} />
+            <Route path="/activities" element={<Activities/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+      
     </div>
   );
 }
