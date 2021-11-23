1737282a613805335a9cc3e716a448551de07dc9
Barbara <barbara.vieira99@gmail.com>
2021-11-23 11:03:49 +0000

    parallax atividades

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,13 +1,36 @@
 import React from "react";
 import ActivityCard from "../ActivityCard/ActivityCard";
 import "./Activities.css";
+import LocomotiveScroll from "locomotive-scroll";
 
 function Activities() {
+    window.addEventListener('DOMContentLoaded', (event) => {
+
+    const element = document.querySelector(".activities-container");
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('[data-scroll-container]'),
+       
+            direction: 'horizontal',
+            smooth: true,
+            lerp: 0.02,
+            tablet: {
+                smooth: true
+            },
+            smartphone: {
+                smooth: true
+            }
+      });
+
+   
+    });
+
   return (
-    <div className="activities-container">
-      <h2>Que atividades posso realizar?</h2>
-      <div className="activities">
-        <ActivityCard
+    <div className="activities-container" data-scroll-container  >
+      <h2 >Que atividades posso realizar?</h2>
+
+      <div className="activities"data-scroll-section      data-scroll-direction="horizontal"
+>
+        <ActivityCard 
           title={"Nome da atividade"}
           text={
             "blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentiumoptio, eaque rerum! Provident similique accusantium nemo autem. Veritatisobcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam"
@@ -21,8 +44,37 @@ function Activities() {
           }
           order={0}
         />
+
+<ActivityCard
+          title={"Nome da atividade"}
+          text={
+            "blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentiumoptio, eaque rerum! Provident similique accusantium nemo autem. Veritatisobcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam"
+          }
+          order={1}
+        /><ActivityCard
+        title={"Nome da atividade"}
+        text={
+          "blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentiumoptio, eaque rerum! Provident similique accusantium nemo autem. Veritatisobcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam"
+        }
+        order={0}
+      />
+
+<ActivityCard
+        title={"Nome da atividade"}
+        text={
+          "blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentiumoptio, eaque rerum! Provident similique accusantium nemo autem. Veritatisobcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam"
+        }
+        order={1}
+      /><ActivityCard
+      title={"Nome da atividade"}
+      text={
+        "blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentiumoptio, eaque rerum! Provident similique accusantium nemo autem. Veritatisobcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam"
+      }
+      order={0}
+    />
+              </div>
+
       </div>
-    </div>
   );
 }
 
--- src/components/ActivityCard/ActivityCard.js
+++ src/components/ActivityCard/ActivityCard.js
@@ -4,28 +4,33 @@ import example from "./example.png";
 
 function ActivityCard({ title, text, order }) {
   return (
-      <div>
-    {order? (
-        <div className="card">
-
-      <img className="activity-image" src={example} />
-      <div className="activity-content">
-        <h3>{title}</h3>
-        <h4 className="text-content">{text}</h4>
-        <ArrowLink text="Saber mais" color="#9F6F63" />
-      </div>
-        </div>): (
-    <div className="card" style={{marginLeft: "180px"}}>
-
-            <div className="activity-content">
-              <h3>{title}</h3>
-              <h4 className="text-content">{text}</h4>
-              <ArrowLink text="Saber mais" color="#9F6F63" />
-            </div>
-            <img className="activity-image" src={example} />
+    <div data-scroll
+    data-scroll-direction="horizontal"
+    data-scroll-speed={Math.random()*1     } className="card-item" >
+    {order ? (
+          <div className="card-content">
+            <div className="card-inner">
+              <img className="activity-image" src={example} />
+              <div className="activity-content">
+                <h3>{title}</h3>
+                <h4 className="text-content">{text}</h4>
+                <ArrowLink text="Saber mais" color="#9F6F63" />
               </div>
-        )
-    }  
+            </div>
+          </div>
+      ) : (
+        <div className="card-content">
+        <div className="card-inner">
+          <div className="activity-content">
+            <h3>{title}</h3>
+            <h4 className="text-content">{text}</h4>
+            <ArrowLink text="Saber mais" color="#9F6F63" />
+          </div>
+          <img className="activity-image" src={example} />
+        </div>
+        </div>
+        
+      )}
     </div>
   );
 }
